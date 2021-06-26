26765e4655b6002a2995f26a6061b1b4a4d2fe7c
Nandaoc <59846823+Nandaoc@users.noreply.github.com>
2021-06-26 18:48:13 -0300

    Criando arquivos necessários (requisitos 1 e 2)

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_adapters/PlaceFragment.java
@@ -0,0 +1,11 @@
+package br.ufmg.coltec.tp.e03_adapters;
+
+import androidx.fragment.app.Fragment;
+
+public class PlaceFragment extends Fragment {
+
+    //construtor
+    public PlaceFragment(){
+        super(R.layout.fragment_places_details);
+    }
+}
